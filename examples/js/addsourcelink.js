46f93238f01d95c02f86c0930b927d2e5d45abe2
Jonathan Lurie <lurie.jo@gmail.com>
2018-05-21 13:30:41 -0400

    hide the header in examples

--- examples/js/addsourcelink.js
+++ examples/js/addsourcelink.js
@@ -4,3 +4,11 @@ let filenameSplit = window.location.pathname.split('/')
 let basename = filenameSplit[filenameSplit.length-1]
 
 sourcelinkEl.href = repoBaseUrl + basename
+
+// hides the header with a GET argument (convenient for iframed demos)
+var url = new URL(window.location.href)
+var hideheader = bool(url.searchParams.get("hideheader"))
+
+if (hideheader) {
+  document.getElementById('header').style.display = 'none'
+}
